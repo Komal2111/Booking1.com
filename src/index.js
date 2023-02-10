e3f015a8f31dc371f3355b2d72f9e66eb14f3789
Komal Garg <komalsnp2111@gmail.com>
2023-02-11 01:17:01 +0530

    new

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { Auth0Provider } from "@auth0/auth0-react";
+// import { Auth0Provider } from "@auth0/auth0-react";
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Auth0Provider
-    domain="dev-xnx6410tbdhu818s.us.auth0.com"
-    clientId="LeSjalOBHrKVQHDEoHMcrSSFZm3VvrC3"
-    authorizationParams={{
-      redirect_uri: window.location.origin
-    }}
-  >
+  // <Auth0Provider
+  //   domain="dev-xnx6410tbdhu818s.us.auth0.com"
+  //   clientId="LeSjalOBHrKVQHDEoHMcrSSFZm3VvrC3"
+  //   authorizationParams={{
+  //     redirect_uri: window.location.origin
+  //   }}
+  // >
     <App />
-  </Auth0Provider>
+);
+  {/* </Auth0Provider> */}
  
     
   
-);
+
